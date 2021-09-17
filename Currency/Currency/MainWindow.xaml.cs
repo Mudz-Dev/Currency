2dabdcf9c7a6f068478273f3cff29543b4cd3ea6
Mudz-Dev-Surface <grayfoxnz@outlook.co.nz>
2021-09-17 20:08:36 +1200

    update

--- Currency/Currency/MainWindow.xaml.cs
+++ Currency/Currency/MainWindow.xaml.cs
@@ -31,15 +31,68 @@ namespace Currency
         {
             this.InitializeComponent();
 
-            LoadCurrencies();
         }
 
-        private void LoadCurrencies()
+        private void NavigationView_Loaded(object sender,RoutedEventArgs e)
         {
+            // Navigates, but does not update the Menu.
+            // ContentFrame.Navigate(typeof(HomePage));
 
-            currencyCalc = new CurrencyCalculator("USD");
-            lstCurrencies.ItemsSource = currencyCalc.Currencies;
+            SetCurrentNavigationViewItem(GetNavigationViewItems(typeof(CurrencyConvert)).First());
+        }
 
+        private void NavigationView_SelectionChanged(NavigationView sender,NavigationViewSelectionChangedEventArgs args)
+        {
+            SetCurrentNavigationViewItem(args.SelectedItemContainer as NavigationViewItem);
+        }
+
+        public void SetCurrentNavigationViewItem(NavigationViewItem item)
+        {
+            if (item == null)
+            {
+                return;
+            }
+
+            if (item.Tag == null)
+            {
+                return;
+            }
+
+            ContentFrame.Navigate(
+            Type.GetType(item.Tag.ToString()),
+            item.Content);
+            NavigationView.Header = item.Content;
+            NavigationView.SelectedItem = item;
+        }
+
+        public List<NavigationViewItem> GetNavigationViewItems()
+        {
+            var result = new List<NavigationViewItem>();
+            var items = NavigationView.MenuItems.Select(i => (NavigationViewItem)i).ToList();
+            items.AddRange(NavigationView.FooterMenuItems.Select(i => (NavigationViewItem)i));
+            result.AddRange(items);
+
+            foreach (NavigationViewItem mainItem in items)
+            {
+                result.AddRange(mainItem.MenuItems.Select(i => (NavigationViewItem)i));
+            }
+
+            return result;
+        }
+
+        public List<NavigationViewItem> GetNavigationViewItems(Type type)
+        {
+            return GetNavigationViewItems().Where(i => i.Tag.ToString() == type.FullName).ToList();
+        }
+
+        public List<NavigationViewItem> GetNavigationViewItems(Type type, string title)
+        {
+            return GetNavigationViewItems(type).Where(ni => ni.Content.ToString() == title).ToList();
+        }
+
+        public NavigationViewItem GetCurrentNavigationViewItem()
+        {
+            return NavigationView.SelectedItem as NavigationViewItem;
         }
 
     }
